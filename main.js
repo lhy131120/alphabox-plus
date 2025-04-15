4bea749006107feec7d1bb697cba2a0bf15493fd
Ray <lhy131120@gmail.com>
2025-04-15 18:00:52 +0800

    update products page

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 document.addEventListener('DOMContentLoaded', () => {
+  
   // Initialize Bootstrap tooltips
-  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
+  // const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  // const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  //   return new bootstrap.Tooltip(tooltipTriggerEl);
+  // });
 
   // Initialize Bootstrap popovers
-  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl);
-  });
+  // const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  // const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  //   return new bootstrap.Popover(popoverTriggerEl);
+  // });
 
   const swiper = new Swiper('.share-swiper', {
   // Optional parameters
@@ -29,28 +30,46 @@ document.addEventListener('DOMContentLoaded', () => {
   breakpoints: {
     992: {
       grid: {
-        rows: 1, 
-      },
+        rows: 1,      },
       slidesPerView: 3,
     },
   },
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-});
+  });
 
-const aboutSwiper = new Swiper('.about .swiper', {
-  spaceBetween: 0,
-  slidesPerView: 1,
-  loop: false,
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  navigation: {
-    nextEl: '.about .swiper-button-next',
-    prevEl: '.about .swiper-button-prev',
-  },
-})
+  const swiperBaseConfig = {
+    spaceBetween: 0,
+    slidesPerView: 1,
+    loop: false,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  }
+
+  if(document.querySelector(".about .swiper")) {
+    new Swiper('.about .swiper', {
+      ...swiperBaseConfig,
+      navigation: {
+        nextEl: '.about .swiper-button-next',
+        prevEl: '.about .swiper-button-prev',
+      },
+    })
+  }
+
+  if(document.querySelector("#products")) {
+    const swipers = document.querySelectorAll('.tab-swiper');
+    swipers.forEach(swiper => {
+      new Swiper(swiper, {
+        ...swiperBaseConfig,
+        navigation: {
+          nextEl: swiper.querySelector(".swiper-button-next"),
+          prevEl: swiper.querySelector(".swiper-button-prev"),
+        },
+      })
+    })
+  }
 
 });
